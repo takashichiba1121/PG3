ca2b08d74cd7cdf5df05cd9cf9f4e400d82df949
takashithiba <k021g1120@g.neec.ac.jp>
2022-11-07 09:24:35 +0900

    2-1 簡略化

--- main.cpp
+++ main.cpp
@@ -4,104 +4,78 @@
 
 using namespace std;
 
-vector<char> GetVectorChar(const char* str)
-{
-	vector<char> VectorStr;
-
-	int i = 0;
-	while (str[i] != '\0') {
-		VectorStr.push_back(str[i]);
-		i++;
-	}
-
-	return VectorStr;
-}
-
 int main(void)
 {
-	list<vector<char>> YamanoteLine;
-	YamanoteLine.push_back(GetVectorChar("Toukyou"));
-	YamanoteLine.push_back(GetVectorChar("Yurakucho"));
-	YamanoteLine.push_back(GetVectorChar("Shimbashi"));
-	YamanoteLine.push_back(GetVectorChar("Hamamatsucho"));
-	YamanoteLine.push_back(GetVectorChar("Tamachi"));
-	YamanoteLine.push_back(GetVectorChar("Shinagawa"));
-	YamanoteLine.push_back(GetVectorChar("Osaki"));
-	YamanoteLine.push_back(GetVectorChar("Gotanda"));
-	YamanoteLine.push_back(GetVectorChar("Meguro"));
-	YamanoteLine.push_back(GetVectorChar("Ebisu"));
-	YamanoteLine.push_back(GetVectorChar("Shibuya"));
-	YamanoteLine.push_back(GetVectorChar("Harajuku"));
-	YamanoteLine.push_back(GetVectorChar("Yoyogi"));
-	YamanoteLine.push_back(GetVectorChar("Shinjuku"));
-	YamanoteLine.push_back(GetVectorChar("Shin-Okubo"));
-	YamanoteLine.push_back(GetVectorChar("Takadanobaba"));
-	YamanoteLine.push_back(GetVectorChar("Mejiro"));
-	YamanoteLine.push_back(GetVectorChar("Ikebukuro"));
-	YamanoteLine.push_back(GetVectorChar("Otsuka"));
-	YamanoteLine.push_back(GetVectorChar("Sugamo"));
-	YamanoteLine.push_back(GetVectorChar("Komagome"));
-	YamanoteLine.push_back(GetVectorChar("Tabata"));
-	YamanoteLine.push_back(GetVectorChar("Nippori"));
-	YamanoteLine.push_back(GetVectorChar("Uguisudani"));
-	YamanoteLine.push_back(GetVectorChar("Ueno"));
-	YamanoteLine.push_back(GetVectorChar("Okachimachi"));
-	YamanoteLine.push_back(GetVectorChar("Akihabara"));
-	YamanoteLine.push_back(GetVectorChar("Kanda"));
+
+	list<const char*> YamanoteLine;
+	YamanoteLine.push_back("Toukyou");
+	YamanoteLine.push_back("Yurakucho");
+	YamanoteLine.push_back("Shimbashi");
+	YamanoteLine.push_back("Hamamatsucho");
+	YamanoteLine.push_back("Tamachi");
+	YamanoteLine.push_back("Shinagawa");
+	YamanoteLine.push_back("Osaki");
+	YamanoteLine.push_back("Gotanda");
+	YamanoteLine.push_back("Meguro");
+	YamanoteLine.push_back("Ebisu");
+	YamanoteLine.push_back("Shibuya");
+	YamanoteLine.push_back("Harajuku");
+	YamanoteLine.push_back("Yoyogi");
+	YamanoteLine.push_back("Shinjuku");
+	YamanoteLine.push_back("Shin-Okubo");
+	YamanoteLine.push_back("Takadanobaba");
+	YamanoteLine.push_back("Mejiro");
+	YamanoteLine.push_back("Ikebukuro");
+	YamanoteLine.push_back("Otsuka");
+	YamanoteLine.push_back("Sugamo");
+	YamanoteLine.push_back("Komagome");
+	YamanoteLine.push_back("Tabata");
+	YamanoteLine.push_back("Nippori");
+	YamanoteLine.push_back("Uguisudani");
+	YamanoteLine.push_back("Ueno");
+	YamanoteLine.push_back("Okachimachi");
+	YamanoteLine.push_back("Akihabara");
+	YamanoteLine.push_back("Kanda");
 
 	cout << 1970 << endl;
 
-	for (list<vector<char>>::iterator itr=YamanoteLine.begin(); itr != YamanoteLine.end();itr++)
+	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++)
 	{
-		vector<char> str = *itr;
-
-		for (vector<char>::iterator it_f = str.begin(); it_f != str.end();it_f++) {
-			cout << *it_f;
-		}
-		cout << endl;
+			cout << *itr << endl;
 	}
 	cout << endl;
 	cout << 2020 << endl;
 
-	for (list<vector<char>>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++)
+	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++)
 	{
-		if (*itr == GetVectorChar("Nippori"))
+		if (*itr == "Nippori")
 		{
-			YamanoteLine.insert(itr,GetVectorChar("Nisi-nippori"));
+			YamanoteLine.insert(itr, "Nisi-nippori");
 			break;
 		}
 	}
 
-	for (list<vector<char>>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++)
+	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++)
 	{
-		vector<char> str = *itr;
-
-		for (vector<char>::iterator it_f = str.begin(); it_f != str.end(); it_f++) {
-			cout << *it_f;
-		}
-		cout << endl;
+		
+			cout << *itr<<endl;
 	}
 
 	cout << endl;
 	cout << 2022 << endl;
 
-	for (list<vector<char>>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++)
+	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++)
 	{
-		if (*itr == GetVectorChar("Shinagawa"))
+		if (*itr == "Shinagawa")
 		{
-			YamanoteLine.insert(itr, GetVectorChar("TakanawaGateway"));
+			YamanoteLine.insert(itr, "TakanawaGateway");
 			break;
 		}
 	}
 
-	for (list<vector<char>>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++)
+	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++)
 	{
-		vector<char> str = *itr;
-
-		for (vector<char>::iterator it_f = str.begin(); it_f != str.end(); it_f++) {
-			cout << *it_f;
-		}
-		cout << endl;
+			cout << *itr<<endl;
 	}
 
 	return 0;
