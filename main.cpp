ad2d3193e62a526cdac8615c5ec0180151998c4d
takashithiba <k021g1120@g.neec.ac.jp>
2022-11-14 10:00:57 +0900

    2-2　終了

--- main.cpp
+++ main.cpp
@@ -1,82 +1,78 @@
 #include<iostream>
-#include<list>
-#include<vector>
+#include<stdlib.h>
 
 using namespace std;
 
+//単方向リストの構造体の定義
+typedef struct CELL {
+	int returnDate;
+	int loanDate;
+	char name[20];
+	struct CELL* next;
+}CELL;
+
+//データを追加する関数のプロトタイプ宣言
+void create(CELL* front,int loanDate,int returnDate, const char* name);
+//一覧を表示する関数のプロトタイプ宣言
+void index(CELL* front);
+
 int main(void)
 {
+	char name[]="YamadaTarou";
+	int loanDate;
+	int returnDate;
+	//先頭セルの宣言
+	CELL front;
+	front.next = nullptr;
 
-	list<const char*> YamanoteLine;
-	YamanoteLine.push_back("Toukyou");
-	YamanoteLine.push_back("Yurakucho");
-	YamanoteLine.push_back("Shimbashi");
-	YamanoteLine.push_back("Hamamatsucho");
-	YamanoteLine.push_back("Tamachi");
-	YamanoteLine.push_back("Shinagawa");
-	YamanoteLine.push_back("Osaki");
-	YamanoteLine.push_back("Gotanda");
-	YamanoteLine.push_back("Meguro");
-	YamanoteLine.push_back("Ebisu");
-	YamanoteLine.push_back("Shibuya");
-	YamanoteLine.push_back("Harajuku");
-	YamanoteLine.push_back("Yoyogi");
-	YamanoteLine.push_back("Shinjuku");
-	YamanoteLine.push_back("Shin-Okubo");
-	YamanoteLine.push_back("Takadanobaba");
-	YamanoteLine.push_back("Mejiro");
-	YamanoteLine.push_back("Ikebukuro");
-	YamanoteLine.push_back("Otsuka");
-	YamanoteLine.push_back("Sugamo");
-	YamanoteLine.push_back("Komagome");
-	YamanoteLine.push_back("Tabata");
-	YamanoteLine.push_back("Nippori");
-	YamanoteLine.push_back("Uguisudani");
-	YamanoteLine.push_back("Ueno");
-	YamanoteLine.push_back("Okachimachi");
-	YamanoteLine.push_back("Akihabara");
-	YamanoteLine.push_back("Kanda");
+	while (true) {
+		scanf_s("%s", name,20);
+		scanf_s("%d", &loanDate);
+		scanf_s("%d", &returnDate);
 
-	cout << 1970 << endl;
 
-	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++)
-	{
-			cout << *itr << endl;
-	}
-	cout << endl;
-	cout << 2020 << endl;
+		//最後尾にセルを追加
+		create(&front, loanDate, returnDate, name);
 
-	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++)
-	{
-		if (*itr == "Nippori")
-		{
-			YamanoteLine.insert(itr, "Nisi-nippori");
-			break;
-		}
+		//リスト一覧の表示
+		index(&front);
 	}
 
-	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++)
-	{
-		
-			cout << *itr<<endl;
-	}
-
-	cout << endl;
-	cout << 2022 << endl;
-
-	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++)
-	{
-		if (*itr == "Shinagawa")
-		{
-			YamanoteLine.insert(itr, "TakanawaGateway");
-			break;
-		}
-	}
-
-	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++)
-	{
-			cout << *itr<<endl;
-	}
 
 	return 0;
+}
+
+//データを追加する関数のプロトタイプ宣言
+void create(CELL* front, int loanDate, int returnDate, const char* name)
+{
+	CELL* newSELL = nullptr;
+
+	//新規作成するセル分のメモリを確保する
+	newSELL = (CELL*)malloc(sizeof(CELL));
+
+	if (newSELL!=NULL)
+	{
+		strcpy_s(newSELL->name, 8, name);
+		newSELL->loanDate = loanDate;
+		newSELL->returnDate = returnDate;
+		newSELL->next = nullptr;
+	}
+
+	//最後(最新)のセルのアドレスの一つ目の処理は引数から持ってきた
+	//リストのうち最初のセルのアドレスが該当する
+	while (front->next != nullptr) {
+		front = front->next;
+	}
+	front->next = newSELL;
+}
+
+//一覧を表示する関数のプロトタイプ宣言
+void index(CELL* front) {
+	while (front->next != nullptr)
+	{
+		front = front->next;
+		printf("氏名/%s\n", front->name);
+		printf("貸出日/%d\n", front->loanDate);
+		printf("返却日/%d\n", front->returnDate);
+	}
 }
