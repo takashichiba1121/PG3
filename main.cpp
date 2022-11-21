d8c256f944a649b73a2efce54ecc73b439541d6b
takashithiba <k021g1120@g.neec.ac.jp>
2022-11-21 12:15:19 +0900

    2-3 課題途中

--- main.cpp
+++ main.cpp
@@ -5,41 +5,41 @@ using namespace std;
 
 //単方向リストの構造体の定義
 typedef struct CELL {
-	int val;
+	char* station;
 	struct CELL* prev;
 	struct CELL* next;
 }CELL;
 
 //挿入したいセルのアドレスを取得
 CELL* getInswrtCellAddress(CELL* endCELL, int iterator);
-//データを追加する関数のプロトタイプ宣言
-void create(CELL* front, int val);
+//データを末尾に追加する関数のプロトタイプ宣言
+void push_back(CELL* front, char* station);
+
+//データを指定したセルに追加する関数のプロトタイプ宣言
+void create(CELL* front, char* station);
+
 //一覧を表示する関数のプロトタイプ宣言
 void index(CELL* front);
 
 int main(void)
 {
-	int val;
+	char* station=nullptr;
 	//先頭セルの宣言
 	CELL head;
-	head.val = 0;
+	head.station;
 	head.next = nullptr;
 	head.prev = nullptr;
 	int iterator;
+	scanf_s("%s", station,20);
+	scanf_s("%d", &iterator);
 
-	while (true) {
-		scanf_s("%d", &val);
-		scanf_s("%d", &iterator);
+	CELL* insertCell = getInswrtCellAddress(&head, iterator);
 
-		CELL* insertCell = getInswrtCellAddress(&head, iterator);
+	//最後尾にセルを追加
+	create(insertCell, station);
 
-		//最後尾にセルを追加
-		create(insertCell, val);
-
-		//リスト一覧の表示
-		index(&head);
-	}
-
+	//リスト一覧の表示
+	index(&head);
 
 	return 0;
 }
@@ -58,16 +58,40 @@ CELL* getInswrtCellAddress(CELL* endCELL, int iterator) {
 }
 
 //データを追加する関数のプロトタイプ宣言
-void create(CELL* currentCell, int val)
+void push_back(CELL* currentCell, char* station)
 {
 	CELL* newSELL = nullptr;
 
 	//新規作成するセル分のメモリを確保する
 	newSELL = (CELL*)malloc(sizeof(CELL));
 
-	newSELL->val = val;
-	newSELL->prev = currentCell;
-	newSELL->next = nullptr;
+	if (newSELL != NULL)
+	{
+		strcpy_s(newSELL->station,16, station);
+		newSELL->prev = currentCell;
+		newSELL->next = nullptr;
+	}
+
+	if (currentCell->next) {
+		CELL* nextCell = currentCell->next;
+		nextCell->prev = newSELL;
+	}
+}
+
+//データを追加する関数のプロトタイプ宣言
+void create(CELL* currentCell, char* station)
+{
+	CELL* newSELL = nullptr;
+
+	//新規作成するセル分のメモリを確保する
+	newSELL = (CELL*)malloc(sizeof(CELL));
+
+	if (newSELL != NULL)
+	{
+		newSELL->station = station;
+		newSELL->prev = currentCell;
+		newSELL->next = nullptr;
+	}
 
 	if (currentCell->next) {
 		CELL* nextCell = currentCell->next;
@@ -83,7 +107,7 @@ void index(CELL* endCell) {
 		endCell = endCell->next;
 		printf("%d", no);
 		printf("%p", endCell->prev);
-		printf("%5d", endCell->val);
+		printf("%5d", endCell->station);
 		printf("(%p)", endCell);
 		printf("%p/n", endCell->next);
 		no++;
